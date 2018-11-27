a66dd47cf6159c375bc6d7d61bd8cce5210277b8
jinadi yasiruka <jinyyash@gmail.com>
2018-11-28 05:19:31 +0530

    g

--- dentalClinic/application/views/doctor_view.php
+++ dentalClinic/application/views/doctor_view.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from fvdthemes.com/html/dentalclinic/doctors.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 14 Nov 2018 11:14:41 GMT -->
+<!-- Mirrored from fvdthemes.com/html/dentalclinic/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 14 Nov 2018 11:12:46 GMT -->
 <head>
 <meta charset="utf-8">
-<
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
 <!-- SITE TITLE -->
 <title>DentalClinic - Responsive Clinic HTML Template</title>
 
@@ -24,12 +26,21 @@
 </head>
 
 <body data-spy="scroll" data-target=".navbar">
+<div class="preloader" id="preloader"> <img src="<?php echo base_url();?>assets/images/preloader.gif" alt="" /> </div>
+
+<!-- =========================
+     TOP BAR
+============================== -->
 <div class="top-bar" id="top-bar">
   <div class="container">
-    <div class="row">
+    <div class="row"> 
+      
+      <!-- TOP BAR LEFT -->
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <div class="top-bar-adress"> <i class="flaticon-navigation-arrow"></i> Henderson Blvd PO Box 356012, Tampa, FL 54452 </div>
+        <div class="top-bar-adress"> <i class="flaticon-navigation-arrow"></i> No 35,Anagarka Darmapala Road,Matara,Srilanka </div>
       </div>
+      
+      <!-- TOP BAR RIGHT -->
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
         <div class="top-bar-social"> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-google-plus"></i></a> <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-skype"></i></a> </div>
         <div class="top-bar-mail"> <i class="fa fa-envelope"></i> <a href="#">office@dental.com</a> </div>
@@ -37,9 +48,18 @@
     </div>
   </div>
 </div>
+<!-- =========================
+     END TOP BAR   
+============================== --> 
+
+<!-- =========================
+     HEADER
+============================== -->
 <div class="header" id="header">
   <div class="container">
-    <div class="row">
+    <div class="row"> 
+      
+      <!-- HEADER LOGO -->
       <div class="col-lg-4 col-md-4 col-sm-5 col-xs-12">
         <div class="header-logo"> <a href="#"><img src="<?php echo base_url();?>assets/images/logo.svg" alt="" />
             <span class="bold600 color-child-6">
@@ -50,17 +70,29 @@
             </span>
           </a> </div>
       </div>
+      
+      <!-- HEADER BUTTONS -->
       <div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
-        <div class="header-button"> <a href="#bookform1" class="fancybox-2 btn btn-default">
+
+        <div class="header-button" > <a href="<?php echo base_url();?>index.php/client/login" class="fancybox-2 btn btn-default">
+
             <span class="plus">
               +
             </span>
             MAKE AN APPOINTMENT</a> </div>
-        <div class="header-phone"> <i class="fa fa-phone"></i> 120 852-4632 </div>
+        <div class="header-phone"> <i class="fa fa-phone"></i> +9441 224 0399 </div>
       </div>
-    </div>
+
+
   </div>
 </div>
+<!-- =========================
+     END HEADER
+============================== --> 
+
+<!-- =========================
+     MAIN MENU
+============================== -->
 <div class="top-menu" id="top-menu">
   <div class="container">
     <div class="row">
@@ -80,38 +112,14 @@
           </div>
         </div>
         <ul class="nav navbar-nav">
-          <li><a href="index.html">HOME</a></li>
+          <li class="active"><a href="index.html">HOME</a></li>
           <li><a href="about.html">ABOUT US</a></li>
-          <li class="dropdown"> <a data-toggle="dropdown" href="#">SERVICES</a>
-            <ul class="dropdown-menu with-bg" role="menu">
-              <li><a href="services.html">Oral exams</a></li>
-              <li><a href="services.html">Teeth whitening</a></li>
-              <li><a href="services.html">Teeth cleaning</a></li>
-              <li><a href="services.html">X-rays</a></li>
-              <li><a href="services.html">Crowns</a></li>
-              <li><a href="services.html">Fillings and bridges</a></li>
-              <li><a href="services.html">Implants</a></li>
-              <li><a href="services.html">Extractions</a></li>
-              <li><a href="services.html">Root canals</a></li>
-              <li><a href="services.html">Surgical implant placements</a></li>
-            </ul>
+          <li > <a  href="<?php echo base_url();?>index.php/service/services">SERVICES</a>
           </li>
-          <li class="dropdown active"> <a data-toggle="dropdown" href="#">OUR DOCTORS</a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="doctors.html">Our doctors</a></li>
-              <li><a href="doctor.html">Doctor</a></li>
-            </ul>
+          <li > <a  href="<?php echo base_url();?>index.php/admin/all_doctors">OUR DOCTORS</a>
+      
           </li>
-          <li class="dropdown"> <a data-toggle="dropdown" href="#">BLOG</a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="blog1.html">Right sidebar</a></li>
-              <li><a href="blog2.html">Left sidebar</a></li>
-              <li><a href="blog3.html">No sidebar</a></li>
-              <li><a href="blog4.html">Column right sidebar</a></li>
-              <li><a href="blog_item.html">Post with sidebar</a></li>
-              <li><a href="blog_item2.html">Post no sidebar</a></li>
-            </ul>
-          </li>
+
           <li class="dropdown"> <a data-toggle="dropdown" href="#">PAGES</a>
             <ul class="dropdown-menu" role="menu">
               <li><a href="gallery1.html">Gallery 1</a></li>
@@ -767,29 +775,7 @@
 <!-- =========================
     COLOR SWITHER
 ============================== -->
-<div class="color-sw-open" id="color-sw-open"> <i class="fa fa-gear"></i> </div>
-<div class="color-sw" id="color-sw">
-  <div class="color-sw-header"> Style switcher
-    <span class="color-sw-close" id="color-sw-close">
-      <i class="fa fa-close"></i>
-    </span>
-  </div>
-  <div class="color-sw-body">
-    <p>Choose color style</p>
-    <div class="color-sw-item active" id="blue">
-      <div></div>
-    </div>
-    <div class="color-sw-item" id="violet">
-      <div></div>
-    </div>
-    <div class="color-sw-item" id="red">
-      <div></div>
-    </div>
-    <div class="color-sw-item" id="green">
-      <div></div>
-    </div>
-  </div>
-</div>
+
 <!-- =========================
     END COLOR SWITHER
 ============================== -->
